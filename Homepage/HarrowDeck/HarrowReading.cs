e853c034b47f0e94f2029dac3aefedadb25b0a39
Sarah Caldow <sarah.caldow@protolabs.com>
2018-07-18 16:40:01 -0500

    fixed traditional hand, displays significant cards

--- Homepage/HarrowDeck/HarrowReading.cs
+++ Homepage/HarrowDeck/HarrowReading.cs
@@ -9,29 +9,81 @@ namespace Homepage.HarrowDeck
     public class HarrowReading
     {
 		private const int TraditionalNumCards = 9;
+	    public Dictionary<string, string> Opposites;
 
 	    public HarrowReading()
 	    {
+		    Opposites = new Dictionary<string, string>()
+		    {
+			    { "Good", "Evil"},
+			    { "Evil", "Good"},
+			    { "Chaotic", "Lawful"},
+			    { "Lawful", "Chaotic"}
+		    };
 	    }
 
-	    public TraditionalHand TraditionalReading(HarrowDeck deck)
+	    public Dictionary<string, HarrowCard> TraditionalReading(HarrowDeck deck)
 	    {
-			var hand = new TraditionalHand();
+			var hand = new Dictionary<string, HarrowCard>();
 		    var shuffle = deck.Shuffle(TraditionalNumCards);
 
-		    hand.LawfulGood = deck.Deck[shuffle[0]];
-		    hand.NeutralGood = deck.Deck[shuffle[1]];
-		    hand.ChaoticGood = deck.Deck[shuffle[2]];
+		    hand.Add("Lawful Good", deck.Deck[shuffle[0]]);
+			hand.Add("Neutral Good", deck.Deck[shuffle[1]]);
+		    hand.Add("Chaotic Good", deck.Deck[shuffle[2]]);
 
-		    hand.LawfulNeutral = deck.Deck[shuffle[3]];
-		    hand.TrueNeutral = deck.Deck[shuffle[4]];
-		    hand.ChaoticNeutral = deck.Deck[shuffle[5]];
+		    hand.Add("Lawful Neutral", deck.Deck[shuffle[3]]);
+		    hand.Add("True Neutral", deck.Deck[shuffle[4]]);
+		    hand.Add("Chaotic Neutral", deck.Deck[shuffle[5]]);
 
-		    hand.LawfulEvil = deck.Deck[shuffle[6]];
-		    hand.NeutralEvil = deck.Deck[shuffle[7]];
-		    hand.ChaoticEvil = deck.Deck[shuffle[8]];
+		    hand.Add("Lawful Evil", deck.Deck[shuffle[6]]);
+		    hand.Add("Neutral Evil", deck.Deck[shuffle[7]]);
+		    hand.Add("Chaotic Evil", deck.Deck[shuffle[8]]);
 
-			return hand;
+			return SetColor(hand);
+	    }
+
+	    public Dictionary<string, HarrowCard> SetColor(Dictionary<string, HarrowCard> hand)
+	    {
+			foreach (var key in hand.Keys.ToList())
+		    {
+			    var alignment = key.Split(" ");
+			    var card = hand[key];
+			    var valence = alignment[0];
+			    var arousal = alignment[1];
+
+			    var oppValence = Opposites.ContainsKey(valence) ? Opposites[valence] : "Neutral";
+				var oppArousal = Opposites.ContainsKey(arousal) ? Opposites[arousal] : "Neutral";
+
+			    if (card.Alignment.Contains(valence) && card.Alignment.Contains(arousal))
+			    {
+				    card.CardColor = "bright-green";
+			    } else if (card.Alignment.Contains(valence))
+			    {
+				    if (!card.Alignment.Contains(oppArousal))
+				    {
+					    card.CardColor = "light-green";
+					}
+			    } else if (card.Alignment.Contains(arousal))
+			    {
+				    if (!card.Alignment.Contains(oppValence))
+				    {
+					    card.CardColor = "light-green";
+					}
+				} else if (card.Alignment.Contains(oppValence))
+			    {
+				    card.CardColor = "light-red";
+				} else if (card.Alignment.Contains(oppArousal))
+			    {
+				    card.CardColor = "light-red";
+				} else if (card.Alignment.Contains(oppArousal) && card.Alignment.Contains(oppValence))
+			    {
+				    card.CardColor = "bright-red";
+				}
+
+			    hand[key] = card;
+		    }
+
+		    return hand;
 	    }
     }
 }
--- Homepage/Models/HarrowCard.cs
+++ Homepage/Models/HarrowCard.cs
@@ -14,5 +14,7 @@ namespace Homepage.Models
 	    public string Alignment { get; set; }
 
 	    public string Description { get; set; }
+
+		public string CardColor { get; set; }
     }
 }
